cc4c5b24141fd6718d55a77e8ef847a7e460e447
Carlos Calgaro <carloscalgarof@gmail.com>
2018-11-16 19:52:13 -0200

    Adicionado Campos de usuário e produtos

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181112233505) do
+ActiveRecord::Schema.define(version: 20181116212401) do
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.integer "quantity"
+    t.decimal "price"
+    t.string "address"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "stablishments", force: :cascade do |t|
     t.string "address"
@@ -34,6 +43,8 @@ ActiveRecord::Schema.define(version: 20181112233505) do
     t.datetime "remember_created_at"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "address"
+    t.string "phone"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
