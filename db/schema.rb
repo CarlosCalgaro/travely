b904f227db62ca81baf150e429dc6f6b8dd2cbd3
Carlos Calgaro <carloscalgarof@gmail.com>
2018-11-19 20:37:28 -0200

    Adicionada gemas jquery e FontAwesome

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20181118225828) do
+ActiveRecord::Schema.define(version: 20181118231251) do
 
   create_table "cart_products", force: :cascade do |t|
     t.integer "cart_id"
@@ -28,6 +28,15 @@ ActiveRecord::Schema.define(version: 20181118225828) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["user_id"], name: "index_carts_on_user_id"
+  end
+
+  create_table "items", force: :cascade do |t|
+    t.string "name"
+    t.integer "quantity"
+    t.decimal "price"
+    t.datetime "date"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
   create_table "products", force: :cascade do |t|
