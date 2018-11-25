37d0e274faf0ba770014199a0ed451df68216dca
Carlos Calgaro <carloscalgarof@gmail.com>
2018-11-25 16:20:11 -0200

    Adicionado reservas e homepage

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   has_one :cart
-
+  has_many :reservations
   def is_human?
     self.type == "HumanUser" ? true : false
   end
